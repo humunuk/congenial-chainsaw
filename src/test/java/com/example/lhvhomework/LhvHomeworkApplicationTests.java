8b18b9de53eac4d2ffff92e5d9387062123c12ce
Siim Kallari <siim.kallari@gmail.com>
2023-09-30 12:38:37 +0300

    Add ability to run tests inside containers

--- src/test/java/com/example/lhvhomework/LhvHomeworkApplicationTests.java
+++ src/test/java/com/example/lhvhomework/LhvHomeworkApplicationTests.java
@@ -2,13 +2,21 @@ package com.example.lhvhomework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class LhvHomeworkApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:latest");
 
     @Test
     void contextLoads() {
-        assert true;
     }
 
 }
